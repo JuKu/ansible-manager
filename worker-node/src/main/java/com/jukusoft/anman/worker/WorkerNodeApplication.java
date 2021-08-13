ec0899c893523d487f5de529592c6b061bdfbe0f
JuKu <kuenzel.justin@t-online.de>
2021-08-13 23:28:11 +0200

    Improve code quality for sonarqube

--- worker-node/src/main/java/com/jukusoft/anman/worker/WorkerNodeApplication.java
+++ worker-node/src/main/java/com/jukusoft/anman/worker/WorkerNodeApplication.java
@@ -1,12 +1,9 @@
 package com.jukusoft.anman.worker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
