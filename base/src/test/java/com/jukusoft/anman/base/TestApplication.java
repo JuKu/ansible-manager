03a014a0834333aca605213b0fc515935a06c3f7
JuKu <kuenzel.justin@t-online.de>
2021-08-15 01:24:28 +0200

    Fix some junit tests

--- base/src/test/java/com/jukusoft/anman/base/TestApplication.java
+++ base/src/test/java/com/jukusoft/anman/base/TestApplication.java
@@ -1,4 +1,4 @@
-package com.jukusoft.anman.server;
+package com.jukusoft.anman.base;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
