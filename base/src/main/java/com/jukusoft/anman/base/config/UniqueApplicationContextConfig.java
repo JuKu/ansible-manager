187dc3f7e7bc8f27e007fcb3525fc9e38c78066f
JuKu <kuenzel.justin@t-online.de>
2021-08-14 21:12:03 +0200

    Add javadocs

--- base/src/main/java/com/jukusoft/anman/base/config/UniqueApplicationContextConfig.java
+++ base/src/main/java/com/jukusoft/anman/base/config/UniqueApplicationContextConfig.java
@@ -2,8 +2,12 @@ package com.jukusoft.anman.base.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,14 +36,14 @@ public class UniqueApplicationContextConfig implements ApplicationContextAware {
     /**
      * the singleton unique application context.
      */
-    private static org.springframework.context.ApplicationContext applicationContext = null;
+    private static ApplicationContext applicationContext = null;
 
     /**
      * get the current application context.
      *
      * @return application context
      */
-    public static org.springframework.context.ApplicationContext getContext() {
+    public static ApplicationContext getContext() {
         return applicationContext;
     }
 
@@ -49,10 +53,22 @@ public class UniqueApplicationContextConfig implements ApplicationContextAware {
      * @param context application context
      */
     @Override
-    public synchronized void setApplicationContext(final org.springframework.context.ApplicationContext context) {
+    public synchronized void setApplicationContext(final ApplicationContext context) {
         applicationContext = context;
     }
 
+    /**
+     * get a specific bean by class type.
+     *
+     * @param cls requested bean class type
+     * @param <T> class type
+     *
+     * @return requested bean
+     * @throws ClassNotFoundException if the bean class could not be found
+     * @throws NoSuchBeanDefinitionException if no bean of the given type was found
+     * @throws NoUniqueBeanDefinitionException if more than one bean of the given type was found
+     * @throws BeansException if the bean could not be created
+     */
     public static <T> T getBean(Class<T> cls) throws ClassNotFoundException {
         return applicationContext.getBean(cls);
     }
