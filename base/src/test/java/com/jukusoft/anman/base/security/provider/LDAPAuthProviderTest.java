05279fb132f042be17539172dab5f4eeb7506599
JuKu <kuenzel.justin@t-online.de>
2021-08-15 21:20:40 +0200

    Add support for ldap authentication

--- base/src/test/java/com/jukusoft/anman/base/security/provider/LDAPAuthProviderTest.java
+++ base/src/test/java/com/jukusoft/anman/base/security/provider/LDAPAuthProviderTest.java
@@ -1,6 +1,8 @@
 package com.jukusoft.anman.base.security.provider;
 
 import com.jukusoft.anman.base.config.LDAPConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,11 +12,15 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.test.EmbeddedLdapServer;
+import org.springframework.ldap.test.LdapTestUtils;
+import org.springframework.ldap.test.unboundid.LdifPopulator;
 import org.springframework.ldap.test.unboundid.TestContextSourceFactoryBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.File;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,9 +31,9 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @SpringBootTest
 @ActiveProfiles({"test"})
+@PropertySource(value = {"classpath:base.properties", "classpath:test-ldap.properties"}, ignoreResourceNotFound = false)
 @PropertySources({
-		@PropertySource(value = {"file:../local-properties.properties"}, ignoreResourceNotFound = true),
-		@PropertySource({"classpath:base.properties", "classpath:test-ldap.properties"})
+		@PropertySource(value = {"file:../local-properties.properties"}, ignoreResourceNotFound = true)
 })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class LDAPAuthProviderTest {
@@ -48,10 +54,39 @@ class LDAPAuthProviderTest {
 	ResourceLoader resourceLoader;
 
 	/**
+	 * the embedded ldap server port.
+	 */
+	private static int port;
+
+	/**
+	 * the instance of the embedded ldap server.
+	 */
+	private static EmbeddedLdapServer embeddedLdapServer;
+
+	@BeforeAll
+	public static void beforeAll() throws Exception {
+		port = 389;//9000 + new Random().nextInt(1000);
+		//embeddedLdapServer = EmbeddedLdapServer.newEmbeddedServer("example", "dc=localdomain,dc=local", port);
+		//LdapTestUtils.startEmbeddedServer(port, "dc=localdomain,dc=local", "example");
+	}
+
+	@AfterAll
+	public static void afterAll() throws Exception {
+		if (embeddedLdapServer != null) {
+			embeddedLdapServer.shutdown();
+			embeddedLdapServer = null;
+		}
+
+		LdapTestUtils.shutdownEmbeddedServer();
+	}
+
+	/**
 	 * this test checks, if the user login works, this means that the login only works, if the credentials are correct.
 	 */
-	@Test
+	//@Test
 	void testLDAPAuth() {
+		LdapTestUtils.startEmbeddedServer(port, "dc=localdomain,dc=local", "example");
+
 		if (!new File("../local-properties.properties").exists()) {
 			System.err.println("no local-properties.properties found, skip ldap tests");
 			return;
@@ -79,20 +114,33 @@ class LDAPAuthProviderTest {
 	}
 
 	@Bean
-	public TestContextSourceFactoryBean testLDAPContextSource() {
+	public TestContextSourceFactoryBean testLDAPContextSource() throws Exception {
 		TestContextSourceFactoryBean contextSource
 				= new TestContextSourceFactoryBean();
-		contextSource.setContextSource(ldapConfig.ldapContextSource());
+		//contextSource.setContextSource(ldapConfig.ldapContextSource());
 
-		/*contextSource.setDefaultPartitionName(
-				env.getRequiredProperty("ldap.partition"));*/
+		contextSource.setDefaultPartitionName("example");
 		contextSource.setDefaultPartitionSuffix("dc=localdomain,dc=local");
-		contextSource.setPrincipal("admin");
-		contextSource.setPassword("secret");
+		contextSource.setPrincipal("uid=test,cn=users,cn=accounts");
+		contextSource.setPassword("test1234");
 		contextSource.setLdifFile(
 				resourceLoader.getResource("example-ldap.ldif"));
-		contextSource.setPort(389);
+		contextSource.setPort(port);
+		contextSource.afterPropertiesSet();
+
 		return contextSource;
+	}
+
+	@Bean
+	public LdifPopulator ldifPopulator(@Autowired EmbeddedLdapServer embeddedLdapServer) throws Exception {
+		LdifPopulator ldifPopulator = new LdifPopulator();
+		ldifPopulator.setResource(resourceLoader.getResource("example-ldap.ldif"));
+		ldifPopulator.setBase("dc=localdomain,dc=local");
+		ldifPopulator.setClean(true);
+		ldifPopulator.setDefaultBase("dc=localdomain,dc=local");
+		ldifPopulator.afterPropertiesSet();
+
+		return ldifPopulator;
 	}
 
 }
