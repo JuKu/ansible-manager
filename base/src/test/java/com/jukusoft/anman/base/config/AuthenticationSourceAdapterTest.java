765e417e6741687fc9a3091745681d8de6866786
JuKu <kuenzel.justin@t-online.de>
2021-08-16 18:34:20 +0200

    Add some junit tests

---
+++ base/src/test/java/com/jukusoft/anman/base/config/AuthenticationSourceAdapterTest.java
@@ -0,0 +1,27 @@
+package com.jukusoft.anman.base.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ldap.core.AuthenticationSource;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ * test class for AuthenticationSourceAdapter.
+ *
+ * @author Justin Kuenzel
+ */
+public class AuthenticationSourceAdapterTest {
+
+	/**
+	 * check the getter methods that the return the same string as in parameters.
+	 */
+	@Test
+	void testGetter() {
+		AuthenticationSource adapter = new AuthenticationSourceAdapter("test1", "test2");
+		assertEquals("test1", adapter.getPrincipal());
+		assertEquals("test2", adapter.getCredentials());
+	}
+
+}
