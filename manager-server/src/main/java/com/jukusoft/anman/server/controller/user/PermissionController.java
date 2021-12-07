69922d5dba59ad56d1e4e41f3c69f7b7c7c22665
JuKu <kuenzel.justin@t-online.de>
2021-12-07 18:06:37 +0100

    Add labels.json

---
+++ manager-server/src/main/java/com/jukusoft/anman/server/controller/user/PermissionController.java
@@ -0,0 +1,12 @@
+package com.jukusoft.anman.server.controller.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Justin Kuenzel
+ */
+@RestController
+public class PermissionController {
+
+
+}
