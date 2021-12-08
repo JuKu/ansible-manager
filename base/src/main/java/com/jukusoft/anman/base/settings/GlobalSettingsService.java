5bed5ff84987e41eeebf0953b219104fbda210a8
JuKu <kuenzel.justin@t-online.de>
2021-12-09 00:58:35 +0100

    Improve code quality for sonarqube

--- base/src/main/java/com/jukusoft/anman/base/settings/GlobalSettingsService.java
+++ base/src/main/java/com/jukusoft/anman/base/settings/GlobalSettingsService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
