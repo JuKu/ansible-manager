cf0f2abc07c7245b5f420dfede895c6836ed344b
JuKu <kuenzel.justin@t-online.de>
2021-12-15 18:01:25 +0100

    Change TeamController

--- manager-server/src/main/java/com/jukusoft/anman/server/controller/teams/TeamController.java
+++ manager-server/src/main/java/com/jukusoft/anman/server/controller/teams/TeamController.java
@@ -1,8 +1,14 @@
 package com.jukusoft.anman.server.controller.teams;
 
+import com.jukusoft.anman.base.entity.general.CustomerEntity;
 import com.jukusoft.anman.base.teams.TeamDTO;
+import com.jukusoft.anman.base.teams.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +20,9 @@ import java.util.List;
  */
 @RestController("/teams")
 public class TeamController {
+
+	@Autowired
+	private TeamService teamService;
 
 	/**
 	 * return a list with all teams which the user belongs to.
@@ -34,10 +43,31 @@ public class TeamController {
 	 * @return list with all teams of the customer
 	 */
 	@GetMapping(path = "/teams/list-customer-teams")
-	public ResponseEntity<List<TeamDTO>> listAllTeams() {
+	public ResponseEntity<List<TeamDTO>> listAllCustomerTeams() {
 		//TODO: check permissions
 
 		return null;
+	}
+
+	/**
+	 * Create a new customer team.
+	 */
+	@PostMapping(path = "/teams/create-team")
+	public ResponseEntity<String> createTeam(@RequestBody TeamDTO team) {
+		//TODO: check permissions
+
+		//TODO: get customer of current user
+		CustomerEntity customer = null;
+
+		//create teams
+		long createdTeamID = teamService.addTeam(customer, team.title(), team.description());
+
+		if (createdTeamID < 0) {
+			//the team could not be created
+			return new ResponseEntity<>(Long.toString(createdTeamID), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<>(Long.toString(createdTeamID), HttpStatus.CREATED);
 	}
 
 }
