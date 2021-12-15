cf9614bea11b4dbdc2fdb7d64035fdadcf446b97
JuKu <kuenzel.justin@t-online.de>
2021-12-15 17:29:57 +0100

    Change TeamController

---
+++ manager-server/src/main/java/com/jukusoft/anman/server/controller/teams/TeamController.java
@@ -0,0 +1,43 @@
+package com.jukusoft.anman.server.controller.teams;
+
+import com.jukusoft.anman.base.teams.TeamDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * This REST controller is responsible for all teams endpoints.
+ *
+ * @author Justin Kuenzel
+ */
+@RestController("/teams")
+public class TeamController {
+
+	/**
+	 * return a list with all teams which the user belongs to.
+	 *
+	 * @return list with own teams
+	 */
+	@GetMapping(path = "/teams/list-own-teams")
+	public ResponseEntity<List<TeamDTO>> listOwnTeams() {
+		//TODO: check permissions
+
+		return null;
+	}
+
+	/**
+	 * return a list with all teams which are available for this customer.
+	 * The permission "customer.can.see.all.teams" is required to use this endpoint.
+	 *
+	 * @return list with all teams of the customer
+	 */
+	@GetMapping(path = "/teams/list-own-teams")
+	public ResponseEntity<List<TeamDTO>> listAllTeams() {
+		//TODO: check permissions
+
+		return null;
+	}
+
+}
