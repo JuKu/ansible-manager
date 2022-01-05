bb8a1e9fd8dc6f92ac2ffb06ed0de6fc3783edcc
JuKu <kuenzel.justin@t-online.de>
2022-01-05 20:09:01 +0100

    Change maven configuration, add support to write git version information into properties file, see #37

---
+++ base/src/main/java/com/jukusoft/anman/base/version/VersionConfiguration.java
@@ -0,0 +1,27 @@
+package com.jukusoft.anman.base.version;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * This configuration class provides the bean with the version information.
+ * See also: https://www.baeldung.com/spring-git-information .
+ *
+ * @author Justin Kuenzel
+ */
+@Configuration
+public class VersionConfiguration {
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer propsConfig
+				= new PropertySourcesPlaceholderConfigurer();
+		propsConfig.setLocation(new ClassPathResource("git.properties"));
+		propsConfig.setIgnoreResourceNotFound(false);
+		propsConfig.setIgnoreUnresolvablePlaceholders(true);
+		return propsConfig;
+	}
+
+}
---
+++ base/src/main/java/com/jukusoft/anman/base/version/VersionDTO.java
@@ -0,0 +1,12 @@
+package com.jukusoft.anman.base.version;
+
+/**
+ * This class represents the current backend version of the system.
+ *
+ * @author Justin Kuenzel
+ */
+public record VersionDTO(String commitId, String branch, String version) {
+
+	//
+
+}
